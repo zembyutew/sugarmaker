c4f2a095829c61cff34b1522fdef117467b6a7c6
kanon <60179867+decryp2kanon@users.noreply.github.com>
2020-03-23 06:24:04 +0900

    move: drop dirname static

--- build-linux32.sh
+++ build-linux32.sh
@@ -36,7 +36,7 @@ strip -s sugarmaker
 file sugarmaker | grep "statically linked"
 
 # PACKAGE
-RELEASE=sugarmaker-v2.5.0-sugar3-linux32-static
+RELEASE=sugarmaker-v2.5.0-sugar3-linux32
 rm -rf $RELEASE
 mkdir $RELEASE
 cp ./mining-script/sh/*.sh $RELEASE/
